e9d55bec02e5f7f26348abe6528bf0e0ab427853
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-04-29 00:40:08 +0600

    initial commit

---
+++ android/app/src/main/java/minhaz1217/github/io/wavy_app/MainActivity.java
@@ -0,0 +1,13 @@
+package minhaz1217.github.io.wavy_app;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
