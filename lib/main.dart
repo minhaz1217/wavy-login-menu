902f5c7d8cddb008ad6903c5a96b712413b2117c
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-04-29 00:40:31 +0600

    Step 1: Creating the wave at the top

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Wavy Header Login page.",
+      home: WavyHeader(),
+    );
+  }
+
+}
+class WavyHeader extends StatelessWidget {
+  List<Color> orangeGradients = [
+    Color(0xFFFF9844),
+    Color(0xFFFE8853),
+    Color(0xFFFD7267),];
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: TopWaveClipper(),
+      child: Container(
+        height: MediaQuery.of(context).size.height/2.5,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: orangeGradients,
+              begin: Alignment.topRight,
+              end: Alignment.center,
+          ),
+        ),
+      ),
+    );
+
+  }
+}
+class TopWaveClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height);
+    var firstControlPoint = new Offset(size.width/7, size.height - 30);
+    var firstEndPoint = new Offset(size.width/6, size.height/1.5);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
+    var secondControlPoint = Offset(size.width / 5, size.height / 4);
+    var secondEndPoint = Offset(size.width / 1.5, size.height / 5);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+    var thirdControlPoint = Offset(size.width - (size.width / 9), size.height / 6);
+    var thirdEndPoint = Offset(size.width, 0.0);
+    path.quadraticBezierTo(thirdControlPoint.dx, thirdControlPoint.dy,
+        thirdEndPoint.dx, thirdEndPoint.dy);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+
+
+}
